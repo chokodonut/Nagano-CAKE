d5d41d6f296953b5063288ad429f28e0bc7e492d
chokodonut <121842407+chokodonut@users.noreply.github.com>
2023-02-22 13:28:58 +0000

    genre edit end

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,2 +1,30 @@
 class Admin::ItemsController < ApplicationController
+
+  def new
+    @item = Item.new
+  end
+  
+  def create
+    item = Item.new(params_item)
+    item.save
+   #redirect_to admin_item_parh(@item.id)
+  end
+  
+  def index
+  end
+  
+  def update
+  end
+  
+  def edit
+  end
+
+  def show
+  end
+  
+  private
+  
+  def item_params
+    params.require(:item).permit(:name, :introduction, :image)
+  end
 end
