2ff228db3db39235c3624a69c2652fedbc8863a9
chokodonut <121842407+chokodonut@users.noreply.github.com>
2023-02-23 13:18:11 +0000

    admin item index

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -3,28 +3,30 @@ class Admin::ItemsController < ApplicationController
   def new
     @item = Item.new
   end
-  
+
   def create
-    item = Item.new(params_item)
-    item.save
-   #redirect_to admin_item_parh(@item.id)
+    @item = Item.new(item_params)
+    @item.save
+   redirect_to admin_item_parh(@item.id)
   end
-  
+
   def index
+    @items = Item.all
   end
-  
+
   def update
   end
-  
+
   def edit
   end
 
   def show
+    
   end
-  
+
   private
-  
+
   def item_params
-    params.require(:item).permit(:name, :introduction, :image)
+    params.require(:item).permit(:name, :introduction, :image, :price, :genre_id, :is_active)
   end
 end
