7114a19c2a9781dcb2858c7586f655bedba9da8e
chokodonut <121842407+chokodonut@users.noreply.github.com>
2023-03-03 12:47:39 +0000

    レイアウト途中

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -39,7 +39,10 @@ class Public::SessionsController < Devise::SessionsController
   def customer_state
   @customer = Customer.find_by(email: params[:customer][:email])
   return if !@customer
-    if @customer.valid_password?(params[:customer][:password])
+    if @customer.valid_password?(params[:customer][:password]) && (@customer.is_deleted == false)
+      redirect_to  new_customer_registration_path
+    else
+      
     end
   end
 end
