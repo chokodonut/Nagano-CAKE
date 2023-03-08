a167e376300852f5307e8cd616f64f4f593de234
chokodonut <121842407+chokodonut@users.noreply.github.com>
2023-03-08 13:25:48 +0000

    cart_item index

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -8,18 +8,19 @@ class Public::CartItemsController < ApplicationController
   end
 
   def create
-    
-    #amount = cart_item_params[:amount]
-    #item_id = cart_item_params[:item_id]
-    if
+    @cart_item = current_customer.cart_items.new(cart_item_params)
+    @already_cart_item = current_customer.cart_items.find_by(item_id: @cart_item.item_id)
+    if @already_cart_item.present?
+       @cart_item.amount +=  @already_cart_item.amount
+       @already_cart_item.destroy
       #データがある時
-    else
+    #else
       #データがないとき
-      @cart_item = CartItem.new(cart_item_params)
-      @cart_item.customer_id = current_customer.id
+      #@cart_item = CartItem.new(cart_item_params)
+      #@cart_item.customer_id = current_customer.id
       #binding.pry
-      @cart_item.save
     end
+    @cart_item.save
     redirect_to cart_items_path
   end
 
