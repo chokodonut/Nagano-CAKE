9f8cf0111e5933a42bdf5c3c0dab26e9fbed199b
chokodonut <121842407+chokodonut@users.noreply.github.com>
2023-03-12 07:54:23 +0000

    order confirm

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -6,6 +6,16 @@ class Public::OrdersController < ApplicationController
   def create
     @cart_items = current_customer.cart_items.all
     @order = current_customer.orders.new(order_params)
+
+    if  @order.save
+    #     cart_items.each do |cart_item|
+    #       @order_detail = OrderDetail.new
+    #       @order_detail.item_id = cart_item.item_id
+    #       @order_detail.order_id = @order.id
+    #       #@order_detail.
+    #       @order_detail.save
+    redirect_to orders_confirm_path
+    end
   end
 
   def index
@@ -14,7 +24,13 @@ class Public::OrdersController < ApplicationController
   def show
   end
 
-  def comfirm
+  def confirm
+    @order = Order.new(order_params)
+    #byebug
+    @cart_items = current_customer.cart_items.all
+    #address_numberの処理をifで分岐してインスタンス変数を書き換える
+    #confirmにヒデンフィールドのバリューでそのインスタンス変数をいれてcreateアクションへいく
+    
   end
 
   def complete
@@ -23,6 +39,10 @@ class Public::OrdersController < ApplicationController
    private
 
   def order_params
-    params.require(:order).permit(:name, :addres, :postal_code, :payment_method)
+    params.require(:order).permit(:name, :address, :postal_code, :payment_method, :address, :address_name)
+  end
+
+  def address_params
+  params.require(:order).permit(:name, :address)
   end
 end
